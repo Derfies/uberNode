36613171e70f164028230b51fa78cfeb88852d25
derfies <derfies@git>
2017-10-24 19:43:31 +1100

    [EDIT] - Reworked the api a lot. There are now classes that handle inputs and outputs which allow easy access via dictionary key notation. This makes getting / setting input values a lot easier, and reduces the number of methods required for the whole thing to work. The get / set stuff also handles reevalutation of the node graph so everything stays up to date. [EDIT] - Nodes now have input values that can be modified without a node plugged in. This is useful if the input is a constant or some other variable that doesn't come from a node; you don't need to create a variable node just to get a single value into the graph. [EDIT] - Rewrote a lot of the tests.

--- tests/uberNodeTests.py
+++ tests/uberNodeTests.py
@@ -9,53 +9,25 @@ if uberPath not in sys.path:
 from uberNode import UberNode
 
 
-class Input( UberNode ):
-    
-    def __init__( self, value, **kwargs ):
-        UberNode.__init__( self, **kwargs )
-
-        self.addStaticOutput( 'value', value )
-
-    def evaluate( self, **inputs ):
-        return {'value': self.value}
-
-
-class Output( UberNode ):
-    
-    def __init__( self, **kwargs ):
-        UberNode.__init__( self, **kwargs )
-
-        self.addInput( 'value' )
-
-
-class Link( UberNode ):
-
-    def __init__( self, **kwargs ):
-        UberNode.__init__( self, **kwargs )
-
-        self.addInput( 'in' )
-        self.addOutput( 'out' )
-
-    def evaluate( self, **inputs ):
-        return {'out': inputs['in']}
-
-
 class Add( UberNode ):
 
-    def __init__( self, **kwargs ):
-        UberNode.__init__( self, **kwargs )
+    def __init__( self ):
+        UberNode.__init__( self, inputs={'a': 0, 'b': 0} )
 
-        self.addInput( 'a' )
-        self.addInput( 'b' )
-        self.addOutput( 'result' )
-    
-    def evaluate( self, **inputs ):
-        return {
-            'result': inputs['a'] + inputs['b']
-        }
+    def evaluate( self ):
+        self.outputs['result'] = self.inputs['a'] + self.inputs['b']
 
 
-class TestStringMethods( unittest.TestCase ):
+class Variable( UberNode ):
+
+    def __init__( self ):
+        UberNode.__init__( self, inputs={'value': None}, outputs={'result': None} )
+
+    def evaluate( self ):
+        self.outputs['result'] = self.inputs['value']
+
+
+class UberNodeTests( unittest.TestCase ):
 
     def test_parent( self ):
         parent = UberNode()
@@ -71,77 +43,51 @@ class TestStringMethods( unittest.TestCase ):
         child.setParent( parent2 )
         self.assertTrue( child.parent == parent2 and child in parent2.children and child.parent != parent1 and child not in parent1.children )
 
-    def test_addDuplicateInput( self ):
-        node = UberNode()
-        with self.assertRaises( Exception ) as context:
-            node.addInput( 'foo' )
-            node.addInput( 'foo' )
-        self.assertTrue( 'Input "foo" already exists' in context.exception )
+    def test_simple( self ):
+        add = Add()
+        self.assertTrue( add.outputs['result'] == 0 )
 
-    def test_addDuplicateOutput( self ):
-        node = UberNode()
-        with self.assertRaises( Exception ) as context:
-            node.addOutput( 'foo' )
-            node.addOutput( 'foo' )
-        self.assertTrue( 'Output "foo" already exists' in context.exception )
+    def test_changeInputs( self ):
+        add = Add()
+        add.inputs['a'] = 1
+        add.inputs['b'] = 2
+        self.assertTrue( add.outputs['result'] == 3 )
 
-    def test_inputValue( self ):
-        out = Output()
-        in_ = Input( 1 )
-        in_.connect( 'value', out, 'value' )
-        self.assertTrue( out.getInputValue() == 1 )
+    def test_connectInput( self ):
+        add = Add()
+        add.inputs['a'] = 1
+        var = Variable()
+        var.inputs['value'] = 2
+        add.inputs.connect( 'b', var.outputs, 'result' )
+        self.assertTrue( add.outputs['result'] == 3 )
 
-    def test_inputValueChanged( self ):
-        out = Output()
-        in_ = Input( 1 )
-        in_.connect( 'value', out, 'value' )
-        oldVal = out.getInputValue()
-        in_.value = 2
-        newVal = out.getInputValue()
-        self.assertTrue( oldVal == 1 and newVal == 2 )
+    def test_connectOutput( self ):
+        add = Add()
+        add.inputs['a'] = 1
+        var = Variable()
+        var.inputs['value'] = 2
+        var.outputs.connect( 'result', add.inputs, 'b' )
+        self.assertTrue( add.outputs['result'] == 3 )
 
-    def test_outputValue( self ):
-        in_ = Input( 1 )
-        self.assertTrue( in_.getOutputValue() == 1 )
-
-    def test_outputValueChanged( self ):
-        in_ = Input( 1 )
-        oldVal = in_.getOutputValue()
-        in_.value = 2
-        newVal = in_.getOutputValue()
-        self.assertTrue( oldVal == 1 and newVal == 2 )
+    def test_changeInput( self ):
+        add = Add()
+        add.inputs['a'] = 1
+        var = Variable()
+        var.inputs['value'] = 2
+        add.inputs.connect( 'b', var.outputs, 'result' )
+        var.inputs['value'] = 3
+        self.assertTrue( add.outputs['result'] == 4 )
 
     def test_chain( self ):
-        in_ = Input( 1 )
-        lnk = Link()
-        out = Output()
-        in_.connect( 'value', lnk, 'in' )
-        lnk.connect( 'out', out, 'value' )
-        self.assertTrue( out.getInputValue() == 1 )
-
-    def test_add( self ):
-        var1 = Input( 1 )
-        var2 = Input( 2 )
-        add = Add()
-        var1.connect( 'value', add, 'a' )
-        var2.connect( 'value', add, 'b' )
-        self.assertTrue( add.getOutputValue() == 3 )
-
-    def test_addMore( self ):
-        """Two inputs added together, then a third added after."""
-        var1 = Input( 1 )
-        var2 = Input( 2 )
-        add1 = Add()
-        var1.connect( 'value', add1, 'a' )
-        var2.connect( 'value', add1, 'b' )
-        var3 = Input( 3 )
-        add2 = Add()
-        add1.connect( 'result', add2, 'a' )
-        var3.connect( 'value', add2, 'b' )
-        self.assertTrue( add2.getOutputValue() == 6 )
-
-    def test_recurse( self ):
-        pass
+        vars_ = []
+        for i in range( 3 ):
+            var = Variable()
+            if i > 0:
+                prevVar = vars_[len( vars_ ) - 1]
+                var.inputs.connect( 'value', prevVar.outputs, 'result' )
+            vars_.append( var )
+        vars_[0].inputs['value'] = 1
+        self.assertTrue( vars_[len( vars_ ) - 1].outputs['result'] == 1 )
 
     def test_disconnect( self ):
         pass
--- uberNode/uberNode.py
+++ uberNode/uberNode.py
@@ -1,3 +1,55 @@
+class Inputs( object ):
+
+    def __init__( self, node, inputs ):
+        self._node = node
+        self._inputs = inputs
+        self._connections = {}
+
+    def __getitem__( self, *args ):
+        key = args[0]
+        if key in self._connections:
+            output, outputName = self._connections[key]
+            return output[outputName]
+        else:
+            return self._inputs[key]
+
+    def __setitem__( self, *args ):
+        self._inputs.update( {args[0]: args[1]} )
+        self._node.onEvaluate()
+
+    def connect( self, inputName, output, outputName, connectInverse=True ):
+
+        # Add input / output connections. Inputs can only have one connection
+        # whereas outputs can have many.
+        self._connections[inputName] = (output, outputName)
+        if connectInverse:
+            output.connect( outputName, self, inputName, connectInverse=False )
+        self._node.onEvaluate()
+
+
+class Outputs( object ):
+
+    def __init__( self, node, outputs ):
+        self._node = node
+        self._outputs = outputs
+        self._connections = {}
+
+    def __getitem__( self, *args ):
+        return self._outputs[args[0]]
+
+    def __setitem__( self, *args ):
+        self._outputs.update( {args[0]: args[1]} )
+
+    def connect( self, outputName, input_, inputName, connectInverse=True ):
+
+        # Add input / output connections. Inputs can only have one connection
+        # whereas outputs can have many.
+        conns = self._connections.setdefault( outputName, [] )
+        conns.append( (input_, inputName) )
+        if connectInverse:
+            input_.connect( inputName, self, outputName, connectInverse=False )
+
+
 class UberNode( object ):
 
     def __init__( self, **kwargs ):
@@ -5,10 +57,11 @@ class UberNode( object ):
         self.parent = kwargs.get( 'parent' )
         self.children = kwargs.get( 'children', [] )
 
-        self.inputs = []
-        self.outputs = {}
-        self.inputConnections = {}
-        self.outputConnections = {}
+        self.inputs = Inputs( self, kwargs.get( 'inputs', {} ) )
+        self.outputs = Outputs( self, kwargs.get( 'outputs', {} ) )
+
+        # Big question about design here. How do we handle if evaluate is all
+        self.onEvaluate()
 
     def setParent( self, parent ):
         if self.parent is not None:
@@ -19,90 +72,21 @@ class UberNode( object ):
     def append( self, node ):
         node.setParent( self )
 
-    def addInput( self, name ):
-        assert name not in self.inputs, 'Input "{}" already exists'.format( name )
-        self.inputs.append( name )
-
-    def addOutput( self, name, value=None ):
-        assert name not in self.outputs, 'Output "{}" already exists'.format( name )
-        self.outputs[name] = value
-
-    def addStaticOutput( self, name, value=None ):
-
-        cls = type( self )
-
-        def fget( self ):
-            return getattr( self, '_' + name )
-
-        def fset( self, value ):
-            setattr( self, '_' + name, value )
-            self.doEvaluation() 
-        
-        self.addOutput( name, value )
-        setattr( cls, '_' + name, value )
-        setattr( cls, name, property( fget, fset ) )
-
-    def getInputValue( self, name=None ):
-
-        # For convenience. If there is only one input then the name kwarg isn't
-        # necessary.
-        if name is None:
-            assert len( self.inputs ) == 1, 'Must specify an input name'
-            name = self.inputs[0]
-        return self.getInputs()[name]
-
-    def getOutputValue( self, name=None ):
-
-        # For convenience. If there is only one output then the name kwarg isn't
-        # necessary.
-        if name is None:
-            assert len( self.outputs ) == 1, 'Must specify an output name'
-            name = self.outputs.keys()[0]
-        assert name in self.outputs, 'Output "{}" does not exist'.format( name )
-        return self.outputs[name]
-
-    def connect( self, outputName, inputNode, inputName ):
-        assert inputName in inputNode.inputs, 'Input "{}" does not exist'.format( inputName )
-        assert outputName in self.outputs, 'Output "{}" does not exist'.format( outputName )
-
-        # Add input / output connections. Inputs can only have one connection
-        # whereas outputs can have many.
-        outputConnections = self.outputConnections.setdefault( outputName, [] )
-        outputConnections.append( (inputNode, inputName) )
-        inputNode.inputConnections[inputName] = (self, outputName)
-        self.evaluateDirtyNodes()
-
-    def disconnect( self, outputName ):
-        print self.outputConnections.setdefault[outputName]
-
-    def evaluateDirtyNodes( self ):
+    def evaluateDownstreamNodes( self ):
 
         # Evaluate all nodes. TO DO - Make this recursive. Also needs more smarts
         # for complex graphs, I imagine. 
         dirtyNodes = []
-        for outputName, connections in self.outputConnections.items():
-            for connection in connections:
-                node, inputName = connection
-                dirtyNodes.append( node )
+        for outputName, conns in self.outputs._connections.items():
+            for conn in conns:
+                input_, inputName = conn
+                dirtyNodes.append( input_._node )
         for node in list( set( dirtyNodes ) ):
-            node.doEvaluation()
+            node.onEvaluate()
 
-    def getInputs( self ):
-        return {
-            inputName: connection[0].getOutputValue( connection[1] )
-            for inputName, connection in self.inputConnections.items()
-        }
-
-    def doEvaluation( self ):
-        inputs = self.getInputs()
-
-        # Only calculate if the required inputs matches the input connections.
-        if set( inputs.keys() ) == set( self.inputs ):
-            results = self.evaluate( **inputs )
-            assert hasattr( results, 'keys' ) and set( results.keys() ) == set( self.outputs.keys() ), 'Evaluate did not return expected values'
-            for k, v in results.items():
-                self.outputs[k] = v
-                self.evaluateDirtyNodes()
+    def onEvaluate( self ):
+        self.evaluate()
+        self.evaluateDownstreamNodes()
 
     def evaluate( self, **inputs ):
         """Take inputs, run code, produce outputs."""
